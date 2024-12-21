9183681ad12e87dda5807e1f6249bc308f55cdf4
MD INZAMUL HAQUE JOY <inzamulhaque1002@gmail.com>
2024-12-21 19:32:59 +0600

    router added aand tailwinf installed

---
+++ src/router/routes.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <h1>Hellooooo from router</h1>
+    }
+])
+
+export default routes;
